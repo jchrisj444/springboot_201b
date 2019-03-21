12074156dfdd59d14b8c3bd787d15832ae99bdc8
GBTC440011UR <GBTC440011UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-20 21:39:26 -0400

    Initial commit

---
+++ src/main/java/com/jchrisj444/springboot_201b/Springboot201bApplication.java
@@ -0,0 +1,13 @@
+package com.jchrisj444.springboot_201b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot201bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot201bApplication.class, args);
+	}
+
+}
